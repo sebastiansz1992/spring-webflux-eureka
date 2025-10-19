e86f91bcae79d314d181ef4f14294e3ea18ddd58
Sebastian Agudelo Marin <sebascarman@gmail.com>
2025-10-19 18:59:05 -0500

    feat: dynamic port and workspace to use the services in one VS window

---
+++ eureka-server/src/test/java/com/sebastian/springcloud/eurekaserver/app/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.springcloud.eurekaserver.app.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
