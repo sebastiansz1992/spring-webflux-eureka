298dd894a95d278b78206034074aea67dd1cbf89
Sebastian Agudelo Marin <sebascarman@gmail.com>
2025-11-26 10:37:08 -0500

    feat: add oauth service

---
+++ msvc-oauth/src/main/java/com/sebastian/springcloud/msvc/oauth/MsvcOauthApplication.java
@@ -0,0 +1,13 @@
+package com.sebastian.springcloud.msvc.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcOauthApplication.class, args);
+	}
+
+}
---
+++ msvc-oauth/src/test/java/com/sebastian/springcloud/msvc/oauth/MsvcOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.springcloud.msvc.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
